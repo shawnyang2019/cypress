372831d17f4bd7a2729ad7bb7d471444610f5b01
vivi <vivid_xiang@163.com>
2017-07-30 00:42:51 +0800

    build

---
+++ cypress-rbac/src/main/java/com/interesting/cypress/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *
+ * Role Based Access Control
+ *
+ * @author vv
+ * @since 2017/7/30.
+ */
+package com.interesting.cypress;
---
+++ cypress-workflow/src/main/java/com/interesting/cypress/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *
+ * 工作流
+ *
+ * @author vv
+ * @since 2017/7/30.
+ */
+package com.interesting.cypress;
