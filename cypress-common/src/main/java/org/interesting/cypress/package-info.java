d652c959fa899448285e6f522cfae5f2cf83f56f
vivi <vivid_xiang@163.com>
2017-07-30 00:36:12 +0800

    build

---
+++ cypress-common/src/main/java/org/interesting/cypress/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ * 系统级别服务
+ *
+ * @author vv
+ * @since 2017/7/30.
+ */
+package org.interesting.cypress;
---
+++ cypress-job/src/main/java/com/interesting/cypress/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *
+ * 任务分发
+ *
+ * @author vv
+ * @since 2017/7/30.
+ */
+package com.interesting.cypress;
