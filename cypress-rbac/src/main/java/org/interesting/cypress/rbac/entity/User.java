e53a1724af7b5f2f85dd7862a5b7cb0296af8cb6
williamxww <vivid_xiang@163.com>
2017-07-30 11:29:52 +0800

    测试SVN 单模块提交

--- cypress-rbac/src/main/java/org/interesting/cypress/rbac/entity/User.java
+++ cypress-rbac/src/main/java/org/interesting/cypress/rbac/entity/User.java
@@ -6,7 +6,17 @@ package org.interesting.cypress.rbac.entity;
  */
 public class User {
 
+    private Integer id;
+
     private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
